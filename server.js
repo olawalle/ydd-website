a4b765e71796a271698906ce2ccb995b15eac312
Olumyco <olumyco4xt@gmail.com>
2020-11-01 15:31:29 +0100

    Changed back to port 7600

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require('express');
 
 const app = express();
 
-const port = 80;
-
-//const port = process.env.PORT || 7600;
+const port = process.env.PORT || 7600;
 
 app.use(express.static(__dirname + "/out"));
 
