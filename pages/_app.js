9e4cca4df6b1719202355e8ffeaf8fee285ffc0f
Olawale Ariyo <olawalle94@gmail.com>
2020-11-01 14:35:41 +0100

    register fixes

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,11 @@ export default function MyApp({ Component, pageProps }) {
             rel='stylesheet'
             key='google-font-cabin'
           />
+          <link
+            async
+            rel='stylesheet'
+            href='//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css'
+          />
         </Head>
         <Component {...pageProps} />
         <style global jsx>{`
